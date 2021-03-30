9e184ae3d2281e36d58efe492e835644d3bbbe4f
ayoub313 <62844392+ayoub313@users.noreply.github.com>
2021-03-30 03:35:12 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+      $(".box-video").click(function(){
+  $('iframe',this)[0].src += "&amp;autoplay=1";
+  $(this).addClass('open');
+});
